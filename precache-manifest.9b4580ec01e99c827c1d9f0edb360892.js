146ea8d8a197516bef08699328986f05e5f2f50e
hch2904 <hch2904@users.noreply.github.com>
2020-05-31 18:48:40 +0000

    deploy: 15f3393bb694c541f415a3599a9215be33283256

---
+++ precache-manifest.9b4580ec01e99c827c1d9f0edb360892.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4d83415fd0ac505684fc1abbfd7ef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9f36025e66f2d6ab5a",
+    "url": "/static/css/main.4f5aeba4.chunk.css"
+  },
+  {
+    "revision": "03e8f92948afb3e85305",
+    "url": "/static/js/2.ecc7d492.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecc7d492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9f36025e66f2d6ab5a",
+    "url": "/static/js/main.401c54d6.chunk.js"
+  },
+  {
+    "revision": "d4d28d0e54b6b287fe1c",
+    "url": "/static/js/runtime-main.36c8ad59.js"
+  },
+  {
+    "revision": "17e0dd66dd975d2b47292a652e2be395",
+    "url": "/static/media/bg-video.17e0dd66.mp4"
+  },
+  {
+    "revision": "2a44e5ad7542ec1f2785d0c54b834f40",
+    "url": "/static/media/my-hero-img.2a44e5ad.png"
+  }
+]);
