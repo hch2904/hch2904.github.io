19da1836cb7595cb7b7ae95226bcdb698194078d
Himanshu Chanan <himanshu2904@outlook.com>
2022-07-26 13:34:03 -0400

    Updates

---
+++ precache-manifest.7345d9fbdcd608e7eb326c55a55713fe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f82b9d42e847db1ee224c9740f6bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42c4180bbf0bc4401f56",
+    "url": "/static/css/main.91d26904.chunk.css"
+  },
+  {
+    "revision": "601995068222b36b483f",
+    "url": "/static/js/2.025f27e0.chunk.js"
+  },
+  {
+    "revision": "c539cd54423426898ababe6867a114a8",
+    "url": "/static/js/2.025f27e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c4180bbf0bc4401f56",
+    "url": "/static/js/main.91223095.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.91223095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8542608c7ef0a5941084",
+    "url": "/static/js/runtime-main.d4c0879b.js"
+  },
+  {
+    "revision": "17e0dd66dd975d2b47292a652e2be395",
+    "url": "/static/media/bg-video.17e0dd66.mp4"
+  },
+  {
+    "revision": "b1389a4cf7582697ffeb14792b41a90c",
+    "url": "/static/media/hch-avatar.b1389a4c.png"
+  },
+  {
+    "revision": "55b6ed5e56ceeea6c0a237e52214dab3",
+    "url": "/static/media/hch.55b6ed5e.jpg"
+  },
+  {
+    "revision": "fd88e2ed40ae617dfdd8921d9c8c4f13",
+    "url": "/static/media/hero-bg.fd88e2ed.png"
+  },
+  {
+    "revision": "fa569e6bb5ff011f0dddcd9c78eda6d2",
+    "url": "/static/media/live-receipt.fa569e6b.md"
+  },
+  {
+    "revision": "86cfefbfd95e1fc37252d1ff4f52165f",
+    "url": "/static/media/medical.86cfefbf.png"
+  },
+  {
+    "revision": "2a44e5ad7542ec1f2785d0c54b834f40",
+    "url": "/static/media/my-hero-img.2a44e5ad.png"
+  },
+  {
+    "revision": "e768d4227ec709b2f43ebaeb545f7469",
+    "url": "/static/media/not-found.e768d422.png"
+  },
+  {
+    "revision": "51044750959d6f664c264395272ea1ff",
+    "url": "/static/media/receipts.51044750.png"
+  },
+  {
+    "revision": "d045b75c2c3c323f89fb735560be047b",
+    "url": "/static/media/retail-bg.d045b75c.jpg"
+  },
+  {
+    "revision": "441698476d199cf81ea9ba3e3fedd3d0",
+    "url": "/static/media/shopping.44169847.png"
+  }
+]);
